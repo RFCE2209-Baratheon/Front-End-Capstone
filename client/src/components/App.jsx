762461358f0bec9e4b304965bb5470c88796c900
Brian Stern <sterno2510@gmail.com>
2022-10-14 11:23:00 -0400

    Added ratingsSummary and ReviewList components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 const {useState, useEffect} = React;
+import Review from './review/Review.jsx'
 
 
 const App = () => {
   return (
+    <>
   <h1>checking that this loads</h1>
+  <Review />
+  </>
   )
 }
 
--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react';
-
+import ReviewList from './ReviewList.jsx';
+import RatingSummary from './RatingSummary.jsx';
+;
 
 const Review = (props) => {
 
   return (
-
+    <>
+    <ReviewList />
+    <RatingSummary />
+    </>
   )
 }
 
