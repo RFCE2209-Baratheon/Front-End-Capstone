6ef8470d84e67dd333625d6d50bd8a99b7fdee37
Huong <htnguuyen@gmail.com>
2022-10-18 00:20:23 -0400

    Made each thumbnail in sidebar into component

---
+++ client/src/components/overview/Thumbnail.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledThumbnail = styled.img`
+    display: block;
+    height: 80px;
+    width: 60px;
+    border: ${props => props.selected ? '3px solid white' : 'none'};
+    cursor: pointer;
+    margin: 10px;
+  `
+
+const Thumbnail = ( {thumbnail, selected, onClick} ) => {
+  return (
+    <StyledThumbnail src={thumbnail.thumbnail_url} selected={selected} onClick={onClick} />
+  )
+}
+
+export default Thumbnail;
