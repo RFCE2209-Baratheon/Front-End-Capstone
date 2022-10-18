0f6fca22f7800877b08586107c57c499d5fbbf51
Huong <htnguuyen@gmail.com>
2022-10-17 23:37:05 -0400

    Completed 95% of default view for imageGallery

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -1,29 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineExpand } from 'react-icons/ai';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
-const ImageGallery = ({ styleImages }) => {
-
-  const [current, setCurrent] = useState(0);
-  const length = styleImages.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  if (!Array.isArray(styleImages) || length <= 0) {
-    return null;
-  }
-
-  const StyledThumbnail = styled.img`
+const StyledThumbnail = styled.img`
     display: block;
-    height: 100px;
-    width: 75px;
-    border: 1.5px solid black;
+    height: 80px;
+    width: 60px;
+    border: ${props => props.selected ? '2px solid white' : '1.5px solid black'};
     cursor: pointer;
     margin: 10px;
   `
@@ -34,13 +18,16 @@ const ImageGallery = ({ styleImages }) => {
 
   const StyledCarouselImage = styled.img`
     width: 500px;
+    &:hover {
+      cursor:zoom-in;
+    }
   `
 
   // transitions not working
   const StyledCarouselItem = styled.div`
-    opacity: ${(active) => (active ? 1 : 0)};
-    transition-duration: ${(active) => (active ? '1s' : '1s ease')};
-    transform: ${(active) => (active ? 'scale(1.08)' : 0)};
+    opacity: ${props => props.active ? '1' : '0'};
+    transition-duration: ${props => props.active ? '1s' : '1s ease'};
+    transition-property: sliding-vertically;
   `
   const StyledCarousel = styled.section`
     position: relative;
@@ -81,33 +68,97 @@ const ImageGallery = ({ styleImages }) => {
     user-select: none;
   `
 
+  const StyledUpArrow = styled(IoIosArrowUp)`
+    position: absolute;
+    top: -5%;
+    left: 30px;
+    font-size: 2rem;
+    color: #000;
+    z-index: 10;
+    cursor: pointer;
+    user-select: none;
+  `
+
+  const StyledDownArrow = styled(IoIosArrowDown)`
+    position: absolute;
+    top: 100%;
+    left: 30px;
+    font-size: 2rem;
+    color: #000;
+    z-index: 10;
+    cursor: pointer;
+    user-select: none;
+  `
+
+const ImageGallery = ({ styleImages }) => {
+
+  const [current, setCurrent] = useState(0);
+  const length = styleImages.length; // 9
+
+
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(7);
+  const [activeThumbnails, setActiveThumbnails] = useState(styleImages.slice(start, end));
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? length - 1 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? 0 : current - 1);
+  };
+
+  if (!Array.isArray(styleImages) || length <= 0) {
+    return null;
+  }
+
+  const changeThumbnails = () => {
+    setActiveThumbnails(styleImages.slice(start, end));
+  }
+
+  useEffect(() => {
+    changeThumbnails();
+  }, [start, end])
+
+  const upSlide = () => {
+    setStart(start === 0 ? 0 : start - 1);
+    setEnd(end === 7 ? 7 : end - 1);
+  };
+
+  const downSlide = () => {
+    setStart(start === length - 7 ? length - 7 : start + 1);
+    setEnd(end === length ? length : end + 1);
+  };
+
+  if (length > 7) {
+    var verticalScroll = true;
+  }
+
   return (
     <>
       <h3>[Image Gallery]</h3>
 
       <StyledCarousel>
         <StyledExpand />
-        <StyledLeftArrow onClick={prevSlide} />
-        <StyledRightArrow onClick={nextSlide}/>
+        {current !== length -1 && <StyledRightArrow onClick={nextSlide} />}
+        {current !== 0 && <StyledLeftArrow onClick={prevSlide} />}
 
         {styleImages.map((image, index) =>
-          <StyledCarouselItem active={index === current ? true : false} key={index}>
+          <StyledCarouselItem active={index === current} key={index}>
             {index === current && (
-              <StyledCarouselImage src={image.thumbnail_url}></StyledCarouselImage>
+              <StyledCarouselImage src={image.url} />
             )}
           </StyledCarouselItem>
         )}
+
         <StyledThumbnailAlign>
-          {styleImages.map((thumbnail, index) =>
-          <StyledThumbnail src={thumbnail.thumbnail_url} key={index} onClick={() => {
-            setCurrent(index);
-          }}></StyledThumbnail>
+          {verticalScroll && start !== 0 && <StyledUpArrow onClick={upSlide} />}
+          {activeThumbnails.map((thumbnail, index) =>
+            <StyledThumbnail src={thumbnail.thumbnail_url} key={index} selected={index === current} onClick={() => setCurrent(index)} />
           )}
+          {verticalScroll && end !== length && <StyledDownArrow onClick={downSlide} />}
         </StyledThumbnailAlign>
-
       </StyledCarousel>
-
-
     </>
   )
 }
