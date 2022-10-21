0f1cf69996daa8ee7c805a371e6261488ab2d329
Brian Stern <sterno2510@gmail.com>
2022-10-21 12:13:27 -0400

    addressed merge index.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
-<<<<<<< HEAD
 import Review from './review/Review.jsx';
+import Related from './related_items/Related_items.jsx';
+import Overview from './overview/Overview.jsx';
 
 const { useState, useEffect } = React;
-=======
-import Related from './related_items/Related_items.jsx';
-const {useState, useEffect} = React;
-import Overview from './overview/Overview.jsx';
->>>>>>> dd8500097d2aca34f63e17f5dd65bd4a34c554e4
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <h1>checking again to see that this loads</h1>
-      <Review />
-    </>
-  );
-=======
     <div>
       <Overview />
       <Related />
+      <Review />
     </div>
-  )
->>>>>>> dd8500097d2aca34f63e17f5dd65bd4a34c554e4
+  );
 }
 
 export default App;
