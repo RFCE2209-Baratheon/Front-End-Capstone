be7c2973c0b854b9674868d6f73a5f38faed2962
Huong <htnguuyen@gmail.com>
2022-10-20 22:54:17 -0400

    Added global styles component

---
+++ client/src/components/overview/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 5;
+    padding: 0;
+    background: #F5F5F5;
+    font-family: Monospace;
+  }
+`;
+
+export default GlobalStyle;
