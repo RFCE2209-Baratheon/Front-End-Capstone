d69484536e254d37067fd4aad24ba700f61eaf15
Huong <htnguuyen@gmail.com>
2022-10-28 08:55:00 -0400

    Added Toggler and Theme.js

---
+++ client/src/components/Theme.js
@@ -0,0 +1,29 @@
+export const lightTheme = {
+  body: '#F4F4F9',
+  text: '#393D3F',
+  toggleBorder: '#FFF',
+  background: '#F4F4F9',
+  logo: '#62929E',
+  h1: '#62929E',
+  h2: '#62929E',
+  h3: '#393D3F',
+  appBorder: 'solid 100px #F4F4F9',
+  a: '#546A7B',
+  select: '#393D3F',
+  button: 'none'
+}
+
+export const darkTheme = {
+  body: '#363537',
+  text: '#C6C5B9',
+  toggleBorder: '#F4F4F9',
+  background: '#546A7B',
+  logo: '#393D3F',
+  h1: '#F4F4F9',
+  h2: '#F4F4F9',
+  h3: '#F4F4F9',
+  appBorder: 'solid 100px #546A7B',
+  a: '#C6C5B9',
+  select: '#546A7B',
+  button: 'none'
+}
