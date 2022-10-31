f11ec88ac54c1668e55211459e850b9cba6d9403
Andrew <drew.311.a@gmail.com>
2022-10-31 01:53:24 -0400

    Code cleanup
    Spacing/extra lines

--- client/src/components/QA/LoadMoreQs.jsx
+++ client/src/components/QA/LoadMoreQs.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import {PropTypes} from 'prop-types'
 
 const LoadMoreQs = ({loadMore}) => {
-
 //component
   return (
   <button className='loadMore' id='123' onClick={(e) => {loadMore(e)}}> Load More Questions </button>
   )
-
 }
 
 //propTypes
