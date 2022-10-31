5b2fe7c0e34cf43347a804f35a83cc12fcaf7fcb
Andrew <drew.311.a@gmail.com>
2022-10-31 01:56:07 -0400

    Styled component nomenclature change
    The styled component 'Test' has now been renamed to AnswerWrapper to more accurate represent it's function

--- client/src/components/QA/Answer.jsx
+++ client/src/components/QA/Answer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {PropTypes} from 'prop-types'
-import {Questions, HelpfulButton, Test, AnswerStyle} from './assets/styles.js'
+import {Questions, HelpfulButton, AnswerWrapper, AnswerStyle} from './assets/styles.js'
 import {format, parseISO} from 'date-fns'
 import Helpful from './Helpful.jsx'
 import Images from './Images.jsx'
@@ -82,15 +82,13 @@ const Answer = ({questionid, shouldFetchQ, setShouldFetchQ, openAModal, shouldFe
     setRenderA(answers.slice(start, renderA.length + 2))
   }
 
-
-
   //component
   return (
       <>
         {renderA.map(function (currentAnswer, index) {
           return (
           <Questions key={index}>
-          <Test className='test'>
+          <AnswerWrapper>
           <div>
             <AnswerStyle>
             <span className= 'answer'>{` ${currentAnswer.body}`}</span>
@@ -102,7 +100,7 @@ const Answer = ({questionid, shouldFetchQ, setShouldFetchQ, openAModal, shouldFe
               <Helpful helpfulCount={currentAnswer.helpfulness} id={currentAnswer.answer_id} helpfulHandler={helpfulAnswerOnclick} reportHandler={reportAnswerOnclick}/>
             </div>
           </div>
-          </Test>
+          </AnswerWrapper>
           </Questions >
           )
         })}
